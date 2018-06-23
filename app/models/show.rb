0c7f4705a151b4580494b01c38a9bf65a48e6369
Emily S <emily.sugano@gmail.com>
2018-06-23 21:16:36 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating)[0]
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating)[0]
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    binding.pry
+    self.where("ratings > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+
+  end
+
+end
